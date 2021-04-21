74bf11a4003304f63b59669be0765cb02697ed28
lamn5 <noddylam24@gmail.com>
2021-04-21 19:45:06 -0400

    Added database to find inputted entries

--- src/routes/Hackit.tsx
+++ src/routes/Hackit.tsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 type UrlParams = {
     slug: string,
 }
+type Hackit = {
+    name: string,
+    description: string,
+}
+const database = [
+    {
+        name: "dogs",
+        description: "all kinds of puppies",
+
+    }
+]
 const HackitPage = () => {
     const { slug } = useParams<UrlParams>();
+
+    const [hackit, setHackit] = useState<Hackit>();
+
+    useEffect(() => {
+        setHackit(database.find((hackit) => {
+            return hackit.name === slug;
+        }))
+    }, [slug])
 
     return(
         <div>
             <p>
                 I'm a hackit!
             </p>
-            <p>
-                {slug}
-            </p>
+            {hackit ? (
+                <div>
+                    <p>
+                        {hackit.name}
+                    </p>
+                    <p>
+                        {hackit.description}
+                    </p>
+                </div>
+            ) : (
+                <p>
+                    Hackit not found
+                </p>
+            )}
         </div>
     )
 }
